6393daafd10d0761fa720de3cbfc2d59b8b458fa
slowriding <48739803+slowriding@users.noreply.github.com>
2020-05-04 11:04:27 -0400

    Create copy.py

---
+++ copy.py
@@ -0,0 +1,5 @@
+import os
+os.system("xcopy blocks/ C:\Users\slow\Desktop\forge-1.7.10-10.13.4.1558-1.7.10-src\src\main\resources\assets\quariummod\textures/")
+os.system("xcopy items/ C:\Users\slow\Desktop\forge-1.7.10-10.13.4.1558-1.7.10-src\src\main\resources\assets\quariummod\textures/")
+
+input("")
