dbd02915e24de0396f11ca9acd64fff9680bdbe7
slowriding <48739803+slowriding@users.noreply.github.com>
2020-05-05 12:10:50 -0400

    lol

--- copy.py
+++ copy.py
@@ -1,5 +1,5 @@
 import os
-os.system("xcopy blocks/ C:\Users\slow\Desktop\forge-1.7.10-10.13.4.1558-1.7.10-src\src\main\resources\assets\quariummod\textures/")
-os.system("xcopy items/ C:\Users\slow\Desktop\forge-1.7.10-10.13.4.1558-1.7.10-src\src\main\resources\assets\quariummod\textures/")
+os.system("\"xcopy blocks/\" \"C:/Users/slow/Desktop/forge-1.7.10-10.13.4.1558-1.7.10-src/src/main/resources/assets/quariummod/textures/\"  /E /K /O")
+os.system("\"xcopy items/\" \"C:/Users/slow/Desktop/forge-1.7.10-10.13.4.1558-1.7.10-src/src/main/resources/assets/quariummod/textures/\"  /E /K /O")
 
 input("")
